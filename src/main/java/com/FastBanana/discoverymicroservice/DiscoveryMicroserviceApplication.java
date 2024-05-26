95150651afafbe2f75e71c0501b9e48ab46e2bc4
libardo.lozanogambas <libardo.lozano.contractor@bbva.com>
2024-05-25 21:24:16 -0500

    init repo 😎

---
+++ src/main/java/com/FastBanana/discoverymicroservice/DiscoveryMicroserviceApplication.java
@@ -0,0 +1,15 @@
+package com.FastBanana.discoverymicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryMicroserviceApplication.class, args);
+	}
+
+}
